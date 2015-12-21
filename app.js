6fbb58c659745e7dcae7f7cfc80a70c641d3446b
Giga <gkokaia@gmail.com>
2015-12-21 17:15:17 +0400

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.get('/', function (req, res) {
   
-   res.send('<html><head><meta http-equiv="refresh" content="2"></head><body><h2>kkkkkkkkkkkkkkkkkkkk</h2>');
+   res.send('<html><head><meta http-equiv="refresh" content="2"></head><body><h2>ტესტ</h2>');
   
 });
 
