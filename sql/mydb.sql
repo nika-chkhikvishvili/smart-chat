33c266fe899584c6b76d1bbf89c2db911f5052c5
onise gabisonia <gabisonia.onise@gmail.com>
2016-05-05 17:00:26 +0400

    Update and rename mydb (3).sql to mydb.sql

--- sql/mydb.sql
+++ sql/mydb.sql
@@ -1,11 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.1.14
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: May 05, 2016 at 02:51 PM
--- Server version: 5.6.17
--- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
