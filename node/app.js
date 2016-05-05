2941a7306cf69aaf30a392ff5488974958a994fa
root <root@smartchat.cloud.gov.ge>
2016-05-06 01:36:01 +0400

    changed BASE_URL in php CONFIG

--- node/app.js
+++ node/app.js
@@ -7,7 +7,7 @@ http_server.listen(3000);
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : '',
+    password : 'root',
     database : 'mydb'
 });
 connection.connect();
