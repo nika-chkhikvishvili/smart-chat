8cae189fbc40abf68267e05cc0d02d02a70e26e7
root <root@smartchat.cloud.gov.ge>
2016-05-06 01:56:02 +0400

    exit replaced by process.exit(1);

--- node/app.js
+++ node/app.js
@@ -6,9 +6,9 @@ var mysql  = require('mysql');
 http_server.listen(3000);
 var connection = mysql.createConnection({
     host     : 'localhost',
-    user     : 'root',
-    password : 'root',
-    database : 'mydb'
+    user     : 'smartchat',
+    password : 'smartchat',
+    database : 'smartchat',
 });
 connection.connect();
 
@@ -28,7 +28,7 @@ var server  = require('./server.js')({mysql:mysql, connection:connection, chatRo
 // initialite tokens
 connection.query('SELECT `t`.`token`, `t`.`person_id`, `t`.`add_date`, `t`.`expire`, `t`.`expired` ' +
     'FROM `mydb`.`person_tokens` t', function(err, rows, fields) {
-    if(err) exit ;
+    if(err) process.exit(1);
     rows.forEach(function (row){
         onlinePersons[row.token] = {
             userId : row.person_id,
@@ -39,7 +39,7 @@ connection.query('SELECT `t`.`token`, `t`.`person_id`, `t`.`add_date`, `t`.`expi
 
 connection.query('SELECT `chat_id`, `online_user_id`, `repo_id`, `chat_uniq_id` FROM `chats` ' +
     ' where `chat_status_id` = 0', function(err, rows, fields) {
-    if(err) exit ;
+    if(err) process.exit(1);
     rows.forEach(function (row){
         chatRooms[row.chat_uniq_id] = {
             chatId : row.chat_id,
