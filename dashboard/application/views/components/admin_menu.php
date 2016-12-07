f09bb51029be69da00b1ef4ac87186380e130bff
onise gabisonia <gabisonia.onise@gmail.com>
2016-12-07 17:56:00 +0400

    Update admin_menu.php

--- dashboard/application/views/components/admin_menu.php
+++ dashboard/application/views/components/admin_menu.php
@@ -34,20 +34,7 @@
     <li class="has_sub">
         <a href="<?= base_url(); ?>dashboard/persons" class="waves-effect"><i class="md md-share"></i><span>მომხმარებლები</span><span
                 class="pull-right"><i class="md md-add"></i></span></a>
-        <ul>
-            <li class="has_sub">
-                <a href="javascript:void(0);" class="waves-effect"><span>Menu Level 1.1</span> <span class="pull-right"><i
-                            class="md md-add"></i></span></a>
-                <ul style="">
-                    <li><a href="javascript:void(0);"><span>Menu Level 2.1</span></a></li>
-                    <li><a href="javascript:void(0);"><span>Menu Level 2.2</span></a></li>
-                    <li><a href="javascript:void(0);"><span>Menu Level 2.3</span></a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="javascript:void(0);"><span>Menu Level 1.2</span></a>
-            </li>
-        </ul>
+       
     </li>
 </ul>
 <div class="clearfix"></div>
