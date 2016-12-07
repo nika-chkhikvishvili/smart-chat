9ca4e3068ead320ab42ca84416e5fbf6789d705b
onise gabisonia <gabisonia.onise@gmail.com>
2016-12-07 18:10:40 +0400

    Update routes.php

--- dashboard/application/config/routes.php
+++ dashboard/application/config/routes.php
@@ -45,6 +45,7 @@ $route['^(add_institution)(/:any)?$'] = 'dashboard/$0';
 $route['^(add_services)(/:any)?$'] = 'dashboard/$0';
 $route['^(persons)(/:any)?$'] = 'dashboard/$0';
 $route['^(add_person)(/:any)?$'] = 'dashboard/$0';
+$route['^(message_templates)(/:any)?$'] = 'dashboard/$0';
 #$route['^(update_service)(/:any)?$'] = 'dashboard/$0';
 
 /* End of file routes.php */
