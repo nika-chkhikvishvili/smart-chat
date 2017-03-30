5277388afc035c0d323a0a38bd2af2d082cb800b
onise gabisonia <gabisonia.onise@gmail.com>
2017-03-31 03:51:49 +0400

    Add files via upload

--- dashboard/application/views/components/admin_menu.php
+++ dashboard/application/views/components/admin_menu.php
@@ -3,34 +3,34 @@
         <a href="<?= base_url(); ?>dashboard" class="waves-effect active"><i class="md md-home"></i><span> მთავარი გვერდი </span></a>
     </li>
 
-    <li class="has_sub">
-        <a href="#" class="waves-effect"><i class="md md-mail"></i><span>შეტყობინებები</span><span class="pull-right"><i
-                    class="md md-add"></i></span></a>
-        <ul class="list-unstyled">
-            <li><a href="#">შემოსული</a></li>
-            <li><a href="#">მაილის გაგზავნა</a></li>
-            <li><a href="#">შეტყობინების ნახვა</a></li>
-        </ul>
+    <li >
+        <a href="<?= base_url(); ?>dashboard/inbox" class="waves-effect"><i class="md md-mail"></i><span>შეტყობინებები</span></a>
+      
     </li>
 
     <li>
-        <a href="#" class="waves-effect"><i class="md md-event"></i><span> ისტორია </span></a>
+    <a href="#" class="waves-effect"><i class="md md-event"></i><span> ისტორია </span></a>
     </li>
 
     <li class="has_sub">
         <a href="#" class="waves-effect"><i class="md md-palette"></i> <span> ადმინისტრირება </span> <span
-                class="pull-right"><i class="md md-add"></i></span></a>
+        class="pull-right"><i class="md md-add"></i></span></a>
         <ul class="list-unstyled">
-            <li><a href="<?= base_url(); ?>add_institution">უწყებების მართვა</a></li>
-            <li><a href="<?= base_url(); ?>add_services">სერვისების მართვა</a></li>
+            <li><a href="<?= base_url(); ?>institution">უწყებების მართვა</a></li>
+            <li><a href="<?= base_url(); ?>services">სერვისების მართვა</a></li>
             <li><a href="<?= base_url(); ?>persons">მომხმარებლის მართვა</a></li>
-            <li><a href="#">სისტემის მართვა</a></li>
-            <li><a href="<?=base_url();?>message_templates">შაბლონების მართვა</a></li>
-            <li><a href="#">პირადი პარამეტრები</a></li>
+            <li><a href="<?= base_url(); ?>dashboard/system">სისტემის მართვა</a></li>
+            <li><a href="<?=base_url();?>dashboard/message_templates">შაბლონების მართვა</a></li>
+            
+            
         </ul>
     </li>
-
-
-   
+    <li>
+<a href="#" class="waves-effect"><i class="md md-cast-connected"></i><span> ავტომოპასუხე </span></a>
+</li>
+ <li>
+<a href="#" class="waves-effect"><i class="md  md-attach-file"></i><span> ფაილების მენეჯმენტი </span></a>
+</li>
+<li><a href="#"><i class="md ion-android-contact"></i><span>პირადი პარამეტრები</span></a></li>   
 </ul>
 <div class="clearfix"></div>
