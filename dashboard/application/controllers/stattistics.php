c6199dbca28ead3035baf5f13eec4592445a185a
onise gabisonia <gabisonia.onise@gmail.com>
2017-07-12 14:51:47 +0400

    Update stattistics.php

--- dashboard/application/controllers/stattistics.php
+++ dashboard/application/controllers/stattistics.php
@@ -210,7 +210,7 @@ class stattistics extends CI_Controller{
 		$datetime1 = @date_create('2009-10-11'); 
 		$datetime2 = @date_create('2009-10-13'); 
 		$interval = date_diff($datetime1, $datetime2); 
-		echo ( $interval['days'] ); 
+		echo  $interval->days'; 
 		
                  echo "<tr class='warning'>
                         <td class='thick-line'>დღეში საშუალო ვიზიტორების რაოდენობა კატეგორიის მიხედვით</td>
