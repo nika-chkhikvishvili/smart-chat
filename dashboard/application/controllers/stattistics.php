d1f06d9bbd741364be8112fb346610596e6154ca
onise gabisonia <gabisonia.onise@gmail.com>
2017-07-12 14:37:43 +0400

    Update stattistics.php

--- dashboard/application/controllers/stattistics.php
+++ dashboard/application/controllers/stattistics.php
@@ -206,6 +206,7 @@ class stattistics extends CI_Controller{
                         <td class='thick-line text-right'>$get_sql_banlist</td>
                         </tr>";
                */
+		date_default_timezone_set('Asia/Baku');
 		$datetime1 = date_create('2009-10-11'); 
 		$datetime2 = date_create('2009-10-13'); 
 		$interval = date_diff($datetime1, $datetime2); 
