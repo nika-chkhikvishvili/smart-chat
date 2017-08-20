636d2fa1e03e386465d3edcc3708a474be8a2804
Giga Kokaia <gkokaia@gmail.com>
2017-08-20 21:31:48 +0400

    add option to css

--- dashboard/application/views/components/main_head.php
+++ dashboard/application/views/components/main_head.php
@@ -14,7 +14,7 @@
 <link href="<?=base_url();?>assets/css/responsive.css" rel="stylesheet" type="text/css">
 <script src="<?=base_url();?>assets/js/modernizr.min.js"></script>
 
-<link href="<?=base_url();?>assets/chat/css/style.css" rel="stylesheet" type="text/css">
+<link href="<?=base_url();?>assets/chat/css/style.css?1" rel="stylesheet" type="text/css">
 <link href="<?=base_url();?>assets/chat/css/giga.css" rel="stylesheet" type="text/css">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.34.9/css/bootstrap-dialog.min.css" rel="stylesheet" type="text/css" />
 
