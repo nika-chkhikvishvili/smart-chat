ad62127c54eb1dcac2d0abfbd60ef78df57c2bc4
onise gabisonia <gabisonia.onise@gmail.com>
2017-10-19 09:26:48 +0400

    Update history.php

--- dashboard/application/controllers/history.php
+++ dashboard/application/controllers/history.php
@@ -20,7 +20,7 @@ class History extends CI_Controller{
         $this->load->helper('form');
         $this->load->library("pagination");
         $data['persons'] = $this->dashboard_model->get_persons();  
-        $sql_history = $this->dashboard_model->count_history('','','');   
+       // $sql_history = $this->dashboard_model->count_history('','','');   
        
        
                  
@@ -65,7 +65,7 @@ class History extends CI_Controller{
 
         $page = ($this->uri->segment(2)) ? $this->uri->segment(2) : 0;
         $data["links"] = $this->pagination->create_links(); 
-         $data['history'] = $this->dashboard_model->get_all_history($this->input->post('service_id'),$this->input->post('firstname'),$this->input->post('lastname'),$this->input->post('operator_name'),'','',$page);	
+        $data['history'] = $this->dashboard_model->get_all_history($this->input->post('service_id'),$this->input->post('firstname'),$this->input->post('lastname'),$this->input->post('operator_name'),'','',$page);	
         
        $data['get_services'] = $this->dashboard_model->get_all_services();
         $this->load->view('chat_history',$data);
