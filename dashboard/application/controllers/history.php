7253f2e9a3c4cbbd226d83be1c924e43c8b2c8fe
onise gabisonia <gabisonia.onise@gmail.com>
2017-10-19 09:28:15 +0400

    Update history.php

--- dashboard/application/controllers/history.php
+++ dashboard/application/controllers/history.php
@@ -20,7 +20,7 @@ class History extends CI_Controller{
         $this->load->helper('form');
         $this->load->library("pagination");
         $data['persons'] = $this->dashboard_model->get_persons();  
-       // $sql_history = $this->dashboard_model->count_history('','','');   
+        $sql_history = $this->dashboard_model->count_history();   
        
        
                  
