22baacaf665889569658d14a24e07cbd8303459b
onise gabisonia <gabisonia.onise@gmail.com>
2018-02-02 17:22:04 +0400

    Update notready.php

--- dashboard/application/controllers/notready.php
+++ dashboard/application/controllers/notready.php
@@ -40,10 +40,10 @@ class notready extends CI_Controller{
                 <tbody>
                 ';
 				
-		if($_POST['singin_singout']){
+		if($_POST['singin_singout']==1){
 		$byservice = $this->dashboard_model->get_singout_byuser(@$_POST['user_id']);
 	
-	foreach($byservice as $services){
+		foreach($byservice as $services){
 				
 				
 				 
@@ -106,7 +106,7 @@ class notready extends CI_Controller{
 	
 	
 		}
-		else
+		elseif($_POST['singin_singout']==0)
 		{	
 		   $count_interval = "";
 		   
@@ -180,7 +180,70 @@ class notready extends CI_Controller{
 					echo "</tr>";
        
 	}
+	else
+	{
+	$byservice = $this->dashboard_model->get_offline_byuser(@$_POST['user_id']);
+	
+		foreach($byservice as $services){
+				
+				
+				 
+				 if($services['state_id']==0)
+				  {
+					  $x = "<span style='color:#C6BD10;' class='fa fa-power-off'>&nbsp;Signout</span>";
+					  $monishvna = $services['change_date'];
+					  $id = $services['id'];
+					  
+					  $get_out = $this->dashboard_model->get_offline_bygridout(@$_POST['user_id'],$id);
+					  if($get_out['type_id']==1){
+						  $active = "<span style='color:#329305;' class='fa fa-play-circle-o'>&nbsp;online</span>";
+						  $on_active = $get_out['change_date'];
+						
+						$start_date = new DateTime($monishvna);
+						$end_date = new DateTime($on_active);
+						$interval = $start_date->diff($end_date);
+						$hours   = $interval->format('%h'); 
+						$minutes = $interval->format('%i');
+						$full_interval = 'ინტერვალი: '.($hours * 60 + $minutes);
+						$count_interval = $count_interval + ($hours * 60 + $minutes);
+					  }
+					  else
+					  {
+						$active = "<span style='color:#EA6A6A;' class='fa fa-lock'>&nbsp;Signin</span>";
+						$on_active = $get_out['change_date'];
+						
+						$start_date = new DateTime($monishvna);
+						$end_date = new DateTime($on_active);
+						$interval = $start_date->diff($end_date);
+						$hours   = $interval->format('%h'); 
+						$minutes = $interval->format('%i');
+						$full_interval = 'ინტერვალი: '.($hours * 60 + $minutes);
+						$count_interval = $count_interval + ($hours * 60 + $minutes); 
+					  }
+					  
+					  
+					    echo "<tr>";
+						
+						echo "<td class='thick-line'>".$services['first_name']."&nbsp;". $services['last_name']."</td>";
+						echo "<td class='thick-line text-center'>".$x."&nbsp;".$monishvna."</td>";
+						echo "<td class='thick-line text-center'>".$active."&nbsp;".$on_active."</td>"; 
+						echo "<td class='thick-line text-center'>&nbsp;".$full_interval." წუთი</td>";
+						echo "</tr>";
+						
+						
+				  }
+				  else
+				  {
+					  $x = "ხელმისაწვდომია";
+					  $monishvna = $services['change_date'];
+					  $xx = "<span style='color:#F13E12;'>ხელმისაწვდომია</span>";
+					  $monishvna2 = $services['change_date']; 
+					  
+					 
+                       
+				  }	
+	}	
     }
-  
+	}
    
 }
